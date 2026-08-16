17f2c29743e1b6a3cb9933dc1018e84db5ad2910
Baskakov <baskakov.evgeniy.93@gmail.com>
2026-08-16 16:12:30 +0300

    add ifor about me

---
+++ ifor_about_me.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int age = 33;
+float hight = 1.82;
+int engyears = 3;
+char letter = 'e';
+
+int main()
+{
+    printf("My age: %d\n",age);
+    printf("My hight: %f\n", hight);
+    printf("Years of englearning: %d\n", engyears);
+    printf("FirestLetter: %c\n", letter);
+    return 0;
+}
+
